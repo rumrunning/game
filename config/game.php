4f6e01a8d1cbcd0ba6d3b5355c7d372ba0c18a35
Jonathan Dey <jon@jrd.io>
2020-07-23 08:40:12 +0100

    Test difficulty

--- config/game.php
+++ config/game.php
@@ -20,29 +20,29 @@ return [
                 'rewards' => [
                     // There is a 100% chance of getting money when rewards are collected
                     new \App\RumRunning\Rewards\Money(50, 110),
-                    new \App\RumRunning\Rewards\Skill(7, 15),
+                    new \App\RumRunning\Rewards\Skill(13, 17),
 
                     // @todo Implement this
                     // 10% chance of discovering them
                     // (new \App\RumRunning\Rewards\Bullet(1, 15))->setDiscoveryChance(0.1)
                 ],
                 'punishments' => [
-                    new \App\RumRunning\Rewards\Skill(5, 10)
+                    new \App\RumRunning\Rewards\Skill(10, 15)
                 ]
             ],
         ],
         [
             'code' => 'store',
             'description' => 'Steal from a store',
-            'difficulty' => 0.2,
+            'difficulty' => 0.25,
             'outcomes' => [
                 'rewards' => [
                     // There is a 100% chance of getting money when rewards are collected
                     new \App\RumRunning\Rewards\Money(100, 200),
-                    new \App\RumRunning\Rewards\Skill(9, 17),
+                    new \App\RumRunning\Rewards\Skill(15, 19),
                 ],
                 'punishments' => [
-                    new \App\RumRunning\Rewards\Skill(7, 10)
+                    new \App\RumRunning\Rewards\Skill(5, 13)
                 ]
             ],
         ]
